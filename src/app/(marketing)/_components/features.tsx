5cbcb3411f18181252a93e10417d05f385c2456d
AgusjkDev <agusjkdev7@gmail.com>
2024-03-24 12:30:40 -0300

    add svg wrapper

--- src/app/(marketing)/_components/features.tsx
+++ src/app/(marketing)/_components/features.tsx
@@ -1,37 +1,41 @@
-import Image from "next/image";
-
+import {
+    PerfectMatch,
+    RealtimeConnections,
+    SecureEnvironment,
+    type SvgType,
+} from "@/components/svgs";
 import AnimatedGradientBorderCard from "@/components/animated-gradient-border-card";
 import SectionLayout from "./section-layout";
 
 const FEATURES = [
     {
         key: "find-your-perfect-match",
-        imageSrc: "perfect-match.svg",
+        svg: PerfectMatch,
         title: "Find your perfect match",
         description:
             "Discover peope of value with our advanced matchmaking algorithms. Whether you're looking for love, friendship, or companionship.",
     },
     {
         key: "realtime-meaningful-connections",
-        imageSrc: "realtime-connections.svg",
+        svg: RealtimeConnections,
         title: "Realtime meaningful connections",
         description:
             "Connect with others who share your passions. Our app facilitates real-time matchmaking based on mutual preferences, ensuring meaningful connections from the start.",
     },
     {
         key: "safe-and-secure-environment",
-        imageSrc: "secure-environment.svg",
+        svg: SecureEnvironment,
         title: "Safe and secure environment",
         description:
             "Your privacy is our top priority. Our app employs state-of-the-art security measures to ensure a safe and enjoyable experience for all users.",
     },
-] satisfies { key: string; imageSrc: string; title: string; description: string }[];
+] satisfies { key: string; svg: SvgType; title: string; description: string }[];
 
 export default function Features() {
     return (
         <SectionLayout id="features" title="Embrace your love journey">
             <div className="flex flex-wrap items-center justify-center gap-8 xl:gap-16">
-                {FEATURES.map(({ key, imageSrc, title, description }) => (
+                {FEATURES.map(({ key, svg: Svg, title, description }) => (
                     <AnimatedGradientBorderCard
                         key={key}
                         as="article"
@@ -39,14 +43,7 @@ export default function Features() {
                         className="max-w-xs"
                     >
                         <div className="grid h-full w-full grid-rows-3 place-items-center px-6 py-8">
-                            <div className="relative aspect-square w-36">
-                                <Image
-                                    alt={`Illustration for ${title}`}
-                                    src={`/imgs/features/${imageSrc}`}
-                                    fill
-                                    quality={100}
-                                />
-                            </div>
+                            <Svg className="aspect-square w-36" />
 
                             <h3 className="font-bold capitalize">{title}</h3>
 
